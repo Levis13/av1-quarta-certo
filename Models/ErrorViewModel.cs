28b641597f18ee0bcae5ce13e2c9d8d5636e3f40
levis13 <levimoreiraoliveira9@gmailcom>
2022-11-29 23:42:27 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace av1_quarta.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
